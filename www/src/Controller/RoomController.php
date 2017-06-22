4fcbe6b0b68cb547d300bc3350275d9b831e3f25
pixocode@pixel <achain.jeremy@gmail.com>
2017-06-22 15:28:51 +0200

    add room controller actions

--- www/src/Controller/RoomController.php
+++ www/src/Controller/RoomController.php
@@ -8,11 +8,17 @@ class RoomController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('Room/index.html.twig');
+        $rooms = $this->getDoctrine()->getManager()->getRepository('App:Room')->findBy(
+            [], ['name' => 'ASC']
+        );
+        return $this->render('Room/index.html.twig', ['rooms' => $rooms]);
     }
 
     public function roomAction(Room $room)
     {
-        return $this->render('Room/room.html.twig', ['room' => $room]);
+        $messages = $this->getDoctrine()->getManager()->getRepository('App:Message')->findBy(
+            ['roomId' => $room->getId()], ['createdAt' => 'ASC']
+        );
+        return $this->render('Room/room.html.twig', ['room' => $room, 'messages' => $messages]);
     }
 }
